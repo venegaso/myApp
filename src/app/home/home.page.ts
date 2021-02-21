d827841c73f33f71114461ea13ef33cbade5dfd5
JHOAN VENEGAS <venegas_@hotmail.com>
2021-02-20 23:55:43 -0500

    calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  display = 0;
+  save = 0;
+  memory = [];
+  operator = '';
+
   constructor() {}
+
+  num(n){
+    this.memory.push(n);
+    this.display = Number(this.memory.join(''));
+  }
+
+  clear(){
+    this.display = 0;
+    this.memory = [];
+  }
+
+  operation(op){
+    this.save = this.display;
+    this.memory = [];
+    this.operator = op;
+  }
+
+  equals(){
+    if(this.operator == '+'){
+      this.display = this.save + this.display;
+    }else if(this.operator == '-'){
+      this.display = this.save - this.display;
+    }
+    else if(this.operator == 'x'){
+      this.display = this.save * this.display;
+    }
+    else if(this.operator == '÷'){
+      this.display = this.save / this.display;
+    }
+    else if(this.operator == 'x^y'){
+      this.display = Math.pow(this.save,this.display);
+    }
+    else if(this.operator == 'π'){
+      this.display = Math.PI;
+    }
+    else if(this.operator == 'Sin'){
+      this.display =  Math.sin(this.display * Math.PI/180),'°';
+    }
+    else if(this.operator == 'Cos'){
+      this.display = Math.cos(this.display * Math.PI/180),'°';
+    }
+    else if(this.operator == 'Tan'){
+      this.display = Math.tan(this.display * Math.PI/180),'°';
+    }
+  }
 
 }
