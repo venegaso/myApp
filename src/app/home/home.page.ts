bc91ca7b8d3fc5d45167eff9eaaad4d9cb1d7fa4
venegaso <venegas_@hotmail.com>
2021-03-28 00:23:44 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   display = 0;
@@ -43,19 +44,19 @@ export class HomePage {
       this.display = this.save / this.display;
     }
     else if(this.operator == 'x^y'){
-      this.display = Number(Math.pow(this.save,this.display));
+      this.display = Math.pow(this.save,this.display);
     }
     else if(this.operator == 'π'){
-      this.display = Number(Math.PI);
+      this.display = Math.PI;
     }
     else if(this.operator == 'Sin'){
-      this.display =  Number(Math.sin(this.display * Number(Math.PI/180));
+      this.display =  Math.sin(this.display * Math.PI/180);
     }
     else if(this.operator == 'Cos'){
-      this.display = Number(Math.cos(this.display * Number(Math.PI/180)));
+      this.display = Math.cos(this.display * Math.PI/180);
     }
     else if(this.operator == 'Tan'){
-      this.display = Number(Math.tan(this.display * Number(Math.PI/180)));
+      this.display = Math.tan(this.display * Math.PI/180);
     }
   }
 
